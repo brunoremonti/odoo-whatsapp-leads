fa79e860b2dda73f46fa6b9dce745eadb3aae743
brunoremonti <brunoremonti@gmail.com>
2025-04-18 20:37:45 -0300

    Update __manifest__.py

--- whatsapp_leads/__manifest__.py
+++ whatsapp_leads/__manifest__.py
@@ -1,1 +1,13 @@
-from . import models
+{
+    'name': 'WhatsApp Leads',
+    'version': '15.0.1.0.0',
+    'summary': 'Botão de WhatsApp para Leads com mensagens personalizadas',
+    'sequence': 10,
+    'depends': ['crm'],
+    'data': [
+        'views/crm_lead_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
